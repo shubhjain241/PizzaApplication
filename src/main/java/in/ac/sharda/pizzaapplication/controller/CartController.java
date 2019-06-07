60696f3a1483b86d27e9ec125702413b05c8a85c
shubhjain241 <shubhjain241@gmail.com>
2019-06-07 08:49:39 +0530

    5.behaviour in cartcontroller to list items

--- src/main/java/in/ac/sharda/pizzaapplication/controller/CartController.java
+++ src/main/java/in/ac/sharda/pizzaapplication/controller/CartController.java
@@ -1,10 +1,22 @@
 package in.ac.sharda.pizzaapplication.controller;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import in.ac.sharda.pizzaapplication.domain.Product;
+//4.) CREATED A REST CONTROLLER 
+
 @RestController
+
 public class CartController {
-	// 4.) CREATED A REST CONTROLLER  
+	// 5.) ADDED A LIST OF CART HAVING PRODUCT
+	private List<Product> products = new ArrayList<>();
 
-
+	public void addProduct(Product product) {
+		this.products.add(product);
+	}
+	
+	
+	  
 }
