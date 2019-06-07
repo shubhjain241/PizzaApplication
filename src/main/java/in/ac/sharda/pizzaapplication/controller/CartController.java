c2b2f9482ce50729ceb680c9787d26b6760f502d
shubhjain241 <shubhjain241@gmail.com>
2019-06-07 08:46:15 +0530

    4.CartController

---
+++ src/main/java/in/ac/sharda/pizzaapplication/controller/CartController.java
@@ -0,0 +1,10 @@
+package in.ac.sharda.pizzaapplication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartController {
+	// 4.) CREATED A REST CONTROLLER  
+
+
+}
